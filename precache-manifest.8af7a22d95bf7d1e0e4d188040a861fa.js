970aeeb3757b090c749425343d6195ecc4c77eb0
Yashpal <yash.pal@altudo.co>
2020-10-05 17:29:11 +0530

    working

---
+++ precache-manifest.8af7a22d95bf7d1e0e4d188040a861fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a4adf369b242d314331206622b023c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88544713bc92ae8c8ccd",
+    "url": "/static/css/main.48258d0f.chunk.css"
+  },
+  {
+    "revision": "0999ccc8c95aa22b8f04",
+    "url": "/static/js/2.4b35e6fc.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/static/js/2.4b35e6fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88544713bc92ae8c8ccd",
+    "url": "/static/js/main.e3d44554.chunk.js"
+  },
+  {
+    "revision": "0ab8fa4becc1f3a4d575",
+    "url": "/static/js/runtime-main.d019264f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
